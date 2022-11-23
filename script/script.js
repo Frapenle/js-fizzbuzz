0d9a04f79f2370e55bd267df1795a30677ad70ef
francescotrudu <francescotrudu84@gmail.com>
2022-11-23 16:37:01 +0100

    Fix console.log output if divisible by 3

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ for (i = 1; i <= 100; i++) {
 
     // if i is divisible by 3 or 5 console.log "FizzBuzz"
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("Fizz");
+        console.log("FizzBuzz");
     } 
     // if i is divisible by 5 console.log "Buzz"
     else if ( i % 5 === 0) {
@@ -11,7 +11,7 @@ for (i = 1; i <= 100; i++) {
     } 
     // if i is divisible by 3 console.log "Fizz"
     else if (i % 3 === 0) {
-        console.log("FizzBuzz");
+        console.log("Fizz");
     } 
     // in tutti gli altri casi console.log i
     else {
