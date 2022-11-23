e8560edb564838e436813572adbcbb0448332501
francescotrudu <francescotrudu84@gmail.com>
2022-11-23 16:34:45 +0100

    Add comments, fix some minor bugs

--- script/script.js
+++ script/script.js
@@ -1,11 +1,20 @@
+// add for  loop to count from 1 to 100
 for (i = 1; i <= 100; i++) {
-    console.log(i);
 
+    // if i is divisible by 3 or 5 console.log "FizzBuzz"
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("Fizz");
-    } else if ( i % 5 === 0) {
+    } 
+    // if i is divisible by 5 console.log "Buzz"
+    else if ( i % 5 === 0) {
         console.log("Buzz");
-    } else if (i % 3 === 0) {
+    } 
+    // if i is divisible by 3 console.log "Fizz"
+    else if (i % 3 === 0) {
         console.log("FizzBuzz");
+    } 
+    // in tutti gli altri casi console.log i
+    else {
+        console.log(i);
     }
 }
